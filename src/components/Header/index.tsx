7e3913a28845f78e3fb161231409a9c69aa01b26
Isaias Nhantumbo Junior <isaiasnhantumbo54@gmail.com>
2023-06-14 23:19:38 +0200

    :sparkles: added Header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,21 @@
+import { Scroll, Timer } from 'phosphor-react';
+import { NavLink } from 'react-router-dom';
+
+import LogoIgnite from '../../assets/logo-ignite.svg';
 import { Container } from './styles';
 
 export function Header() {
   return (
     <Container>
-      <h1>Header</h1>
+      <img src={LogoIgnite} alt="Dois triângulos" />
+      <nav>
+        <NavLink to="/" title="Home">
+          <Timer size={24} />
+        </NavLink>
+        <NavLink to="/history" title="History">
+          <Scroll size={24}/>
+        </NavLink>
+      </nav>
     </Container>
   );
 }
