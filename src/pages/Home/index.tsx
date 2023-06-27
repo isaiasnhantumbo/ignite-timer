22ffc98cae2d7ce3784625f8b74d8b8c96cf3c17
Isaias Nhantumbo Junior <isaiasnhantumbo54@gmail.com>
2023-06-27 21:17:15 +0200

    :lipstick: added home and history pages

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,9 +1,57 @@
-import { Container } from './styles';
+import { Play } from 'phosphor-react';
+
+import {
+  Container,
+  CountdownContainer,
+  FormContainer,
+  MinutesAmountInput,
+  Separator,
+  StartCountdownButton,
+  TaskInput,
+} from './styles';
 
 export function Home() {
   return (
     <Container>
-      <h1>Home</h1>
+      <form action="">
+        <FormContainer>
+          <label htmlFor="task">Vou trabalhar em</label>
+          <TaskInput
+            type="text"
+            id="task"
+            placeholder="Dê um nome para o seu projecto"
+            list="task-suggestions"
+          />
+          <datalist id="task-suggestions">
+            <option value="Teste" />
+            <option value="Estudar React" />
+            <option value="Estudar Nodejs" />
+          </datalist>
+
+          <label htmlFor="minutesAmount">durante</label>
+          <MinutesAmountInput
+            type="number"
+            id="minutesAmount"
+            placeholder="00"
+            step={5}
+            min={5}
+            max={60}
+          />
+
+          <span>minutos.</span>
+        </FormContainer>
+        <CountdownContainer>
+          <span>0</span>
+          <span>0</span>
+          <Separator>:</Separator>
+          <span>0</span>
+          <span>0</span>
+        </CountdownContainer>
+        <StartCountdownButton type="submit">
+          <Play size={24} />
+          Começar
+        </StartCountdownButton>
+      </form>
     </Container>
   );
 }
